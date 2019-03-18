085e635e7431d0d8bde20471bff2b559a5ce0ac9
lauren wright <wright.lauren20@gmail.com>
2019-03-18 11:07:07 -0400

    conditionals

---
+++ conditionals.php
@@ -0,0 +1,23 @@
+<?php
+$a = 5;
+$b = 10;
+
+// if ($a == $b) {
+//     echo ' values are equal ';
+// } elseif ($a  < $b) {
+//     echo ' $a is less than $b '; 
+// } elseif ($a > $b){
+//     echo ' $a is greater than $b ';
+// } else {
+//     echo ' values are NOT equal ';
+// }
+
+$score = 20;
+
+if ($score > 59){
+    echo ' you completed the level! ';
+} else {
+    echo ' Please try again ';
+}
+
+?>
