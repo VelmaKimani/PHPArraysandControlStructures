92e2bee05c4c2ac8e4b5975bfc46117624289f18
lauren wright <wright.lauren20@gmail.com>
2019-03-18 11:58:00 -0400

    nested conditionals

--- conditionals.php
+++ conditionals.php
@@ -2,16 +2,17 @@
 $a = 5;
 $b = 10;
 
-if ($a == $b){
-} else {
-    echo ' values are NOT equal ';
-}
-if (($a == $b) == false) {
-  echo ' values are NOT equal ';
-}
-if ($a <> $b) {
-  echo ' values are NOT equal ';
-}
+
+// if ($a == $b){
+// } else {
+//     echo ' values are NOT equal ';
+// }
+// if (($a == $b) == false) {
+//   echo ' values are NOT equal ';
+// }
+// if ($a <> $b) {
+//   echo ' values are NOT equal ';
+// }
 // if ($a == $b) {
 //     echo ' values are equal ';
 // } elseif ($a  < $b) {
@@ -29,5 +30,17 @@ if ($a <> $b) {
 // } else {
 //     echo ' Please try again ';
 // }
+
+$num = 100;
+
+if ($num >= 10){
+    if ($num <= 1000){
+        echo ' Your number is within the range. ';
+    } else {
+        echo ' Your number is greater than 1000, NOT within the range. ';
+    }
+}else {
+    echo ' Your  number is less than 10, NOT within the range ';
+}
 
 ?>
